4ecb35534dd22e05e647fa71dc5d07712a93dffb
Aksamrat <samratuap52@gmail.com>
2024-04-28 14:37:17 +0600

    dotenv file ok

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyCUov6gNR-VC7Ki25melIH3wrYozAWysS8',
-  authDomain: 'sketch-craft.firebaseapp.com',
-  projectId: 'sketch-craft',
-  storageBucket: 'sketch-craft.appspot.com',
-  messagingSenderId: '614658242912',
-  appId: '1:614658242912:web:3f6560efca3a329471c605',
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
